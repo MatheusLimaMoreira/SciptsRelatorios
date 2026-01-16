c8d97b38cca8778882831af2eb6d907bfef84ae3
Matheus de Lima <matheus.moreira127@gmail.com>
2026-01-16 12:06:12 -0300

    conclusão da riação do parametro e correção de command

--- OrdemServico/Commands/TableServicoItem.sql
+++ OrdemServico/Commands/TableServicoItem.sql
@@ -1,20 +1,28 @@
-SELECT
-	SI.cServicoItem,
-	SI.cServico,
-	SI.Descricao,
-	SI.Quantidade,
-	SI.ValorUnitarioLiquido,
-	SI.ValorTotalLiquido,
-	SI.cVendedor,
-	PV.NomeRazao,
-	S.cServicoGrupo AS GrupoServico
-FROM TbOpr_ServicoItem SI 
-INNER JOIN TbCad_Vendedor V ON V.cVendedor = SI.cVendedor AND V.cEmpresa = SI.cEmpresa
-INNER JOIN TbCad_Pessoa PV ON PV.cPessoa = V.cPessoa
-INNER JOIN TbOpr_Servico S ON SI.cServico = S.cServico
-WHERE
-	SI.Estado = 0
-	AND SI.cEmpresa = @cEmpresa
-	AND SI.cAtendimento = @cAtendimento
-	AND (@cServicoGrupo = ''
-        OR S.cServicoGrupo = @cServicoGrupo)
+DECLARE @Query AS VARCHAR(MAX);
+
+
+SET @Query = '
+	SELECT
+		SI.cServicoItem,
+		SI.cServico,
+		SI.Descricao,
+		SI.Quantidade,
+		SI.ValorUnitarioLiquido,
+		SI.ValorTotalLiquido,
+		SI.cVendedor,
+		PV.NomeRazao,
+		S.cServicoGrupo AS GrupoServico
+	FROM TbOpr_ServicoItem SI
+	INNER JOIN TbCad_Vendedor V ON V.cVendedor = SI.cVendedor AND V.cEmpresa = SI.cEmpresa
+	INNER JOIN TbCad_Pessoa PV ON PV.cPessoa = V.cPessoa
+	INNER JOIN TbOpr_Servico S ON SI.cServico = S.cServico
+	WHERE
+		SI.Estado = ''''
+		AND SI.cEmpresa = ' + @cEmpresa + '
+		AND SI.cAtendimento = ' + @cAtendimento + '
+';
+
+IF (@cServicoGrupo <> '')
+	SET @Query = @Query + ' AND S.cServicoGrupo = ''' + @cServicoGrupo + '''';
+
+EXECUTE(@Query);
